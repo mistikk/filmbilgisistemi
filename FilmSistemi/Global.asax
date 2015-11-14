f990d9e3933d8c46436e41b405dd96071de2aadf
Mustafa Büyükçelebi <mustafa.buyukcel@gmail.com>
2015-11-14 22:14:28 +0200

    Init Commit

---
+++ FilmSistemi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FilmSistemi.MvcApplication" Language="C#" %>
---
+++ FilmSistemi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilmSistemi.Startup))]
+namespace FilmSistemi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
