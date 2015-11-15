39e97888a83bd8cfec2fb7afe423aad41da472b8
emre-yilmaz <emre.yilmaz10@ogr.sakarya.edu.tr>
2015-11-15 20:06:15 +0200

    ref #2 js dosyası eklendi

--- FilmSistemi/App_Start/BundleConfig.cs
+++ FilmSistemi/App_Start/BundleConfig.cs
@@ -17,7 +17,8 @@ namespace FilmSistemi
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*",
+                        "~/Scripts/modernizr.custom.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
