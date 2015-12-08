5f02dcf9ee579e7c30f368f231c64743c66a9744
Mustafa Büyükçelebi <mustafa@polisoft.com.tr>
2015-12-08 10:00:32 +0200

    Tasarımlar tekrar düzenlendi

---
+++ FilmSistemi/Controllers/MoviePageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmSistemi.Controllers
+{
+    public class MoviePageController : Controller
+    {
+        // GET: MoviePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
