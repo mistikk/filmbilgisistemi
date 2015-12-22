321ddecca106d4353d0daf1e9bb1438152acdb17
Uğur Erdal <ugur@polisoft.com.tr>
2015-12-22 16:38:25 +0200

    revert yapıldı hata giderildi

--- FilmSistemi/Areas/Admin/Controllers/SimagesController.cs
+++ FilmSistemi/Areas/Admin/Controllers/SimagesController.cs
@@ -10,116 +10,107 @@ using FilmSistemi.Models;
 
 namespace FilmSistemi.Areas.Admin.Controllers
 {
-    public class ActorMoviesController : Controller
+    public class SimagesController : Controller
     {
         private FilmSistemiEntities db = new FilmSistemiEntities();
 
-        // GET: Admin/ActorMovies
+        // GET: Admin/Simages
         public ActionResult Index()
         {
-            var actorMovie = db.ActorMovie.Include(a => a.Actors).Include(a => a.Movies);
-            return View(actorMovie.ToList());
+            return View(db.Simage.ToList());
         }
 
-        // GET: Admin/ActorMovies/Details/5
+        // GET: Admin/Simages/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ActorMovie actorMovie = db.ActorMovie.Find(id);
-            if (actorMovie == null)
+            Simage simage = db.Simage.Find(id);
+            if (simage == null)
             {
                 return HttpNotFound();
             }
-            return View(actorMovie);
+            return View(simage);
         }
 
-        // GET: Admin/ActorMovies/Create
+        // GET: Admin/Simages/Create
         public ActionResult Create()
         {
-            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "ActorName");
-            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName");
             return View();
         }
 
-        // POST: Admin/ActorMovies/Create
+        // POST: Admin/Simages/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ActorMovieId,ActorId,MovieId")] ActorMovie actorMovie)
+        public ActionResult Create([Bind(Include = "Sid,İmage,MovieId")] Simage simage)
         {
             if (ModelState.IsValid)
             {
-                db.ActorMovie.Add(actorMovie);
+                db.Simage.Add(simage);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "ActorName", actorMovie.ActorId);
-            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName", actorMovie.MovieId);
-            return View(actorMovie);
+            return View(simage);
         }
 
-        // GET: Admin/ActorMovies/Edit/5
+        // GET: Admin/Simages/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ActorMovie actorMovie = db.ActorMovie.Find(id);
-            if (actorMovie == null)
+            Simage simage = db.Simage.Find(id);
+            if (simage == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "ActorName", actorMovie.ActorId);
-            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName", actorMovie.MovieId);
-            return View(actorMovie);
+            return View(simage);
         }
 
-        // POST: Admin/ActorMovies/Edit/5
+        // POST: Admin/Simages/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ActorMovieId,ActorId,MovieId")] ActorMovie actorMovie)
+        public ActionResult Edit([Bind(Include = "Sid,İmage,MovieId")] Simage simage)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(actorMovie).State = EntityState.Modified;
+                db.Entry(simage).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "ActorName", actorMovie.ActorId);
-            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName", actorMovie.MovieId);
-            return View(actorMovie);
+            return View(simage);
         }
 
-        // GET: Admin/ActorMovies/Delete/5
+        // GET: Admin/Simages/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ActorMovie actorMovie = db.ActorMovie.Find(id);
-            if (actorMovie == null)
+            Simage simage = db.Simage.Find(id);
+            if (simage == null)
             {
                 return HttpNotFound();
             }
-            return View(actorMovie);
+            return View(simage);
         }
 
-        // POST: Admin/ActorMovies/Delete/5
+        // POST: Admin/Simages/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            ActorMovie actorMovie = db.ActorMovie.Find(id);
-            db.ActorMovie.Remove(actorMovie);
+            Simage simage = db.Simage.Find(id);
+            db.Simage.Remove(simage);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
