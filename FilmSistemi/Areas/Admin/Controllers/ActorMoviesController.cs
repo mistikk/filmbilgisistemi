c42e0e213a81f8d3f25a3c58df65f189e40a3f24
Uğur Erdal <ugur@polisoft.com.tr>
2015-12-22 15:59:37 +0200

    fix all

---
+++ FilmSistemi/Areas/Admin/Controllers/ActorMoviesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FilmSistemi.Models;
+
+namespace FilmSistemi.Areas.Admin.Controllers
+{
+    public class ActorMoviesController : Controller
+    {
+        private FilmSistemiEntities db = new FilmSistemiEntities();
+
+        // GET: Admin/ActorMovies
+        public ActionResult Index()
+        {
+            var actorMovie = db.ActorMovie.Include(a => a.Actors).Include(a => a.Movies);
+            return View(actorMovie.ToList());
+        }
+
+        // GET: Admin/ActorMovies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActorMovie actorMovie = db.ActorMovie.Find(id);
+            if (actorMovie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actorMovie);
+        }
+
+        // GET: Admin/ActorMovies/Create
+        public ActionResult Create()
+        {
+            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "ActorName");
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName");
+            return View();
+        }
+
+        // POST: Admin/ActorMovies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ActorMovieId,ActorId,MovieId")] ActorMovie actorMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ActorMovie.Add(actorMovie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "ActorName", actorMovie.ActorId);
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName", actorMovie.MovieId);
+            return View(actorMovie);
+        }
+
+        // GET: Admin/ActorMovies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActorMovie actorMovie = db.ActorMovie.Find(id);
+            if (actorMovie == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "ActorName", actorMovie.ActorId);
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName", actorMovie.MovieId);
+            return View(actorMovie);
+        }
+
+        // POST: Admin/ActorMovies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ActorMovieId,ActorId,MovieId")] ActorMovie actorMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(actorMovie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "ActorName", actorMovie.ActorId);
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName", actorMovie.MovieId);
+            return View(actorMovie);
+        }
+
+        // GET: Admin/ActorMovies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActorMovie actorMovie = db.ActorMovie.Find(id);
+            if (actorMovie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actorMovie);
+        }
+
+        // POST: Admin/ActorMovies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ActorMovie actorMovie = db.ActorMovie.Find(id);
+            db.ActorMovie.Remove(actorMovie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FilmSistemi/Areas/Admin/Controllers/MovieCategoriesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FilmSistemi.Models;
+
+namespace FilmSistemi.Areas.Admin.Controllers
+{
+    public class MovieCategoriesController : Controller
+    {
+        private FilmSistemiEntities db = new FilmSistemiEntities();
+
+        // GET: Admin/MovieCategories
+        public ActionResult Index()
+        {
+            var movieCategory = db.MovieCategory.Include(m => m.Categories).Include(m => m.Movies);
+            return View(movieCategory.ToList());
+        }
+
+        // GET: Admin/MovieCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieCategory movieCategory = db.MovieCategory.Find(id);
+            if (movieCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movieCategory);
+        }
+
+        // GET: Admin/MovieCategories/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CName");
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName");
+            return View();
+        }
+
+        // POST: Admin/MovieCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MovieCategoryId,MovieId,CategoryId")] MovieCategory movieCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MovieCategory.Add(movieCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CName", movieCategory.CategoryId);
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName", movieCategory.MovieId);
+            return View(movieCategory);
+        }
+
+        // GET: Admin/MovieCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieCategory movieCategory = db.MovieCategory.Find(id);
+            if (movieCategory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CName", movieCategory.CategoryId);
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName", movieCategory.MovieId);
+            return View(movieCategory);
+        }
+
+        // POST: Admin/MovieCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MovieCategoryId,MovieId,CategoryId")] MovieCategory movieCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movieCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CName", movieCategory.CategoryId);
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "MName", movieCategory.MovieId);
+            return View(movieCategory);
+        }
+
+        // GET: Admin/MovieCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieCategory movieCategory = db.MovieCategory.Find(id);
+            if (movieCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movieCategory);
+        }
+
+        // POST: Admin/MovieCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MovieCategory movieCategory = db.MovieCategory.Find(id);
+            db.MovieCategory.Remove(movieCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
