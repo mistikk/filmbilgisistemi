c7957d1bb02e6989bfdde3bac04472d8bea8b984
emreyillmaz <emreyillmaz@gmail.com>
2015-12-25 23:44:40 +0200

    haberler sayfası eklendi

---
+++ FilmSistemi/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using FilmSistemi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmSistemi.Controllers
+{
+    public class NewsController : Controller
+    {
+        FilmSistemiEntities db = new FilmSistemiEntities();
+
+
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
