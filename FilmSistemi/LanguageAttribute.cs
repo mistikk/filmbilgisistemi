3deb38adc81811a97b8cd3ae4f17d26e067b2686
emreyillmaz <emreyillmaz@gmail.com>
2015-12-27 20:18:08 +0200

    fix

---
+++ FilmSistemi/LanguageAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmSistemi
+{
+    public class LanguageAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string lang = filterContext.RouteData.Values["lang"] != null ? filterContext.RouteData.Values["lang"].ToString() : "tr";
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(lang);
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(lang);
+
+            //filterContext.HttpContext.Response.Cookies.Add(new HttpCookie("dil") { Value = lang, Expires = DateTime.Now.AddYears(1) });
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
